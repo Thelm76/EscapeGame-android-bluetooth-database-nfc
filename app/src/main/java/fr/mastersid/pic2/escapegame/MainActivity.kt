c4c71ca7dd2789bba1e4222b85fe8a0caa482a1a
Mateo MARTINEZ <mateo.martinez@etu.univ-rouen.fr>
2021-10-15 15:17:36 +0200

    Wifi starting

--- app/src/main/java/fr/mastersid/pic2/escapegame/MainActivity.kt
+++ app/src/main/java/fr/mastersid/pic2/escapegame/MainActivity.kt
@@ -1,11 +1,83 @@
 package fr.mastersid.pic2.escapegame
 
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.p2p.WifiP2pManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import fr.mastersid.pic2.escapegame.utils.WiFiDirectBroadcastReceiver
 
 class MainActivity : AppCompatActivity() {
+
+    val manager: WifiP2pManager? by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager?
+    }
+
+    var channel: WifiP2pManager.Channel? = null
+    var receiver: BroadcastReceiver? = null
+
+    val intentFilter = IntentFilter().apply {
+        addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        channel = manager?.initialize(this, mainLooper, null)
+        channel?.also { channel ->
+            receiver = WiFiDirectBroadcastReceiver(manager!!, channel, this)
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        manager?.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+
+            override fun onSuccess() {
+                Log.d("WiFi", "YAY")
+            }
+
+            override fun onFailure(reasonCode: Int) {
+                Log.e("Wifi", "nope...")
+            }
+        })
+
     }
+
+    /* register the broadcast receiver with the intent values to be matched */
+    override fun onResume() {
+        super.onResume()
+        receiver?.also { receiver ->
+            registerReceiver(receiver, intentFilter)
+        }
+    }
+
+    /* unregister the broadcast receiver */
+    override fun onPause() {
+        super.onPause()
+        receiver?.also { receiver ->
+            unregisterReceiver(receiver)
+        }
+    }
+
 }
---
+++ app/src/main/java/fr/mastersid/pic2/escapegame/utils/WiFiDirectBroadcastReceiver.kt
@@ -0,0 +1,44 @@
+package fr.mastersid.pic2.escapegame.utils
+
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.wifi.p2p.WifiP2pManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import fr.mastersid.pic2.escapegame.MainActivity
+
+class WiFiDirectBroadcastReceiver(
+    private val manager: WifiP2pManager,
+    private val channel: WifiP2pManager.Channel,
+    private val activity: MainActivity
+) : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val action: String = intent.action!!
+        when (action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                when (state) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        // Wifi P2P is enabled
+                    }
+                    else -> {
+                        // Wi-Fi P2P is not enabled
+                    }
+                }
+            }
+            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                // Call WifiP2pManager.requestPeers() to get a list of current peers
+            }
+            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+                // Respond to new connection or disconnections
+            }
+            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                // Respond to this device's wifi state changing
+            }
+
+        }
+    }
+}
