eabd0345c7d26f6c88d4c2534bc49534d4e7ac1b
BryanBarre <barrebryan289@gmail.com>
2021-11-01 20:11:49 +0100

    theme color and interfaces improved

--- app/src/main/java/fr/mastersid/pic2/escapegame/PopUpWindow.kt
+++ app/src/main/java/fr/mastersid/pic2/escapegame/PopUpWindow.kt
@@ -4,12 +4,8 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
-import android.app.Activity
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.ColorUtils
@@ -19,7 +15,6 @@ class PopUpWindow : AppCompatActivity() {
     private var popupTitle = ""
     private var popupText = ""
     private var popupButton = ""
-    private var darkStatusBar = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,27 +27,11 @@ class PopUpWindow : AppCompatActivity() {
         popupTitle = bundle?.getString("popuptitle", "Title") ?: ""
         popupText = bundle?.getString("popuptext", "Text") ?: ""
         popupButton = bundle?.getString("popupbtn", "Button") ?: ""
-        darkStatusBar = bundle?.getBoolean("darkstatusbar", false) ?: false
 
         // Set the data
         popup_window_text.text = popupText
         popup_window_button.text = popupButton
 
-        // Set the Status bar appearance for different API levels
-        if (Build.VERSION.SDK_INT in 19..20) {
-            setWindowFlag(this, true)
-        }
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // If you want dark status bar, set darkStatusBar to true
-            if (darkStatusBar) {
-                this.window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            }
-            this.window.statusBarColor = Color.TRANSPARENT
-            setWindowFlag(this, false)
-        }
 
         // Fade animation for the background of Popup Window
         val alpha = 100 //between 0-255
@@ -66,8 +45,8 @@ class PopUpWindow : AppCompatActivity() {
 
 
         // Fade animation for the Popup Window
-        popup_window_view_with_border.alpha = 0f
-        popup_window_view_with_border.animate().alpha(1f).setDuration(500).setInterpolator(
+        popup_window_view.alpha = 0f
+        popup_window_view.animate().alpha(1f).setDuration(500).setInterpolator(
             DecelerateInterpolator()
         ).start()
 
@@ -78,16 +57,6 @@ class PopUpWindow : AppCompatActivity() {
         }
     }
 
-    private fun setWindowFlag(activity: Activity, on: Boolean) {
-        val win = activity.window
-        val winParams = win.attributes
-        if (on) {
-            winParams.flags = winParams.flags or WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-        } else {
-            winParams.flags = winParams.flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS.inv()
-        }
-        win.attributes = winParams
-    }
 
 
     override fun onBackPressed() {
@@ -103,7 +72,7 @@ class PopUpWindow : AppCompatActivity() {
         }
 
         // Fade animation for the Popup Window when you press the back button
-        popup_window_view_with_border.animate().alpha(0f).setDuration(500).setInterpolator(
+        popup_window_view.animate().alpha(0f).setDuration(500).setInterpolator(
             DecelerateInterpolator()
         ).start()
 
